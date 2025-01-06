77778c72fa2ec2106be066c80d1e33e79c946a94
Anastasiia Bulba <fedinanastya@gmail.com>
2025-01-06 19:20:10 +0200

    homework one nodejs

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,22 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    // тут я читаю існуючі контакти
+    const contacts = await readContacts();
+
+    // а тут я генерую нові
+    const newContacts = Array.from({ length: number }, createFakeContact);
+
+    // розсипаю все, тобто об'єдную і записую всі назад
+    await writeContacts([...contacts, ...newContacts]);
+
+    console.log(`Successfully generated ${number} new contacts.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error.message);
+  }
+};
 
 generateContacts(5);
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  // читаю файл
+  const data = await fs.readFile(PATH_DB, 'utf-8');
+
+  // повертаю файл
+  return JSON.parse(data);
+};
